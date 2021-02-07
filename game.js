9a0d4d7e3aad96a02450e04cfb930fe5f6f0b964
Shashank Vivek <shashank.vivek@qoncept.at>
2021-02-07 20:52:01 +0530

    added food and snake expansion code

--- game.js
+++ game.js
@@ -4,10 +4,19 @@ import {
   SNAKE_SPEED,
 } from "./snake.js";
 
+import {
+  update as updateFood,
+  draw as drawFood,
+} from "./food.js";
+
 let lastRenderedTime = 0;
 const gameBoard = document.getElementById("game-board");
+const 
 
 function main(currentTime) {
+  if(gameOver){
+    return alert('loose')
+  }
   window.requestAnimationFrame(main);
   const secondSinceLastRender = (currentTime - lastRenderedTime) / 1000;
   if (secondSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -22,9 +31,17 @@ window.requestAnimationFrame(main);
 
 function update() {
   updateSnake();
+  updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+
+function checkDeath(){
+  gameOver = outSideGrid(getSnakeHead()) ||  snakeIntersection();
 }
--- snake.js
+++ snake.js
@@ -1,11 +1,11 @@
 import { getInputDirection } from "./input.js";
 
 export const SNAKE_SPEED = 5;
-const snakeBody = [
-  { x: 10, y: 11 }
-];
+const snakeBody = [{ x: 10, y: 11 }];
+let newSegment = 0;
 
 export function update() {
+  addSegments();
   const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
@@ -22,4 +22,33 @@ export function draw(gameBoard) {
     snakeElement.classList.add("snake");
     gameBoard.appendChild(snakeElement);
   });
+}
+
+export function expandSnake(amount) {
+  newSegment += amount;
+}
+
+export function onSnake(position) {
+  return snakeBody.some((segment) => {
+    return equalPosition(segment, position);
+  });
+}
+
+function equalPosition(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y;
+}
+
+function addSegments() {
+  for (let i = 0; i < newSegment; i++) {
+    snakeBody.push({ ...snakeBody.length[snakeBody.length - 1] });
+  }
+  newSegment = 0;
+}
+
+export function getSnakeHead(){
+    return snakeBody[0]  
+}
+
+export function snakeIntersection(){
+    return onSnake(snakeBody[0], {ignoreHead: true})
 }
